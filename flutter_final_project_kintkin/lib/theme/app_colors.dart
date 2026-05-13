8976716df81fed879dc8d5d99561b56b9f8d856f
NicholasCarlen <99189725+NicholasCarlen@users.noreply.github.com>
2026-05-13 11:49:58 +0700

    simple  changes

--- flutter_final_project_kintkin/lib/theme/app_colors.dart
+++ flutter_final_project_kintkin/lib/theme/app_colors.dart
@@ -9,5 +9,8 @@ class AppColors {
   static const Color text1 = Color(0xFF1A1A1A);
   static const Color text2 = Color(0xFF6B7280);
   static const Color textBG = Color(0xFFB0B5BD);
+  static const Color navBar = Color(0xFFFFCD76);
+  static const Color selectnav = Color(0xFF191970);
   
+
 }
