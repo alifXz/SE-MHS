7e2b20fa13acfbffa85f320aee648f5862f4dcbd
NicholasCarlen <99189725+NicholasCarlen@users.noreply.github.com>
2026-05-17 20:54:19 +0700

    Making claendar plugins and base structure

---
+++ flutter_final_project_kintkin/lib/Pages/schedule_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatefulWidget {
+  const Calendar({super.key});
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  DateTime _focusedDay =  DateTime.now();
+  DateTime? _selectedDay;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Simple Calendar')),
+      body: TableCalendar(
+        focusedDay: _focusedDay, 
+        firstDay: DateTime.utc(2026, 1, 1),
+         lastDay: DateTime(2026, 12, 31),
+
+          selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+          onDaySelected: (selectedDay, focusedDay) {
+          setState(() {
+            _selectedDay = selectedDay;
+            _focusedDay = focusedDay; 
+          });
+        },
+        onPageChanged: (focusedDay) {
+          _focusedDay = focusedDay;
+        },
+      ),
+
+          
+    );
+  }
+}
