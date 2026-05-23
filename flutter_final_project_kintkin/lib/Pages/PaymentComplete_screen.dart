d0dacd299d7cd39d3421047d183ed10ed874d5cf
NicholasCarlen <99189725+NicholasCarlen@users.noreply.github.com>
2026-05-23 12:30:54 +0700

    create payment complete screen

---
+++ flutter_final_project_kintkin/lib/Pages/PaymentComplete_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PaymentcompleteScreen extends StatefulWidget {
+  const PaymentcompleteScreen({super.key});
+
+  @override
+  State<PaymentcompleteScreen> createState() => _PaymentcompleteScreenState();
+}
+
+class _PaymentcompleteScreenState extends State<PaymentcompleteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Column(
+        children: [
+         
+        ],
+      ),
+    );
+  }
+}
