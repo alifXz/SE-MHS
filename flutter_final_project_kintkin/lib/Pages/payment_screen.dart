04cb656a6e7c8f8bde1e90c157e7f4b3169a4f54
marchellochen <marchellochen05@gmail.com>
2026-05-29 02:21:18 +0700

    removed rupiah formatting
    no longer needed, no more hardcode

--- flutter_final_project_kintkin/lib/Pages/payment_screen.dart
+++ flutter_final_project_kintkin/lib/Pages/payment_screen.dart
@@ -18,31 +18,6 @@ class PaymentScreen extends StatefulWidget {
 }
 
 class _PaymentScreen extends State<PaymentScreen> {
-  String? _selectedMethod;
-
-  final int price = 50000;
-  final int tax = 5000;
-  final int serviceFee = 5000;
-
-  int get _total => price + tax + serviceFee;
-
-  final List<Map<String, dynamic>> _paymentMethods = [
-    {'id': 'bca', 'name': 'BCA', 'color': Color(0xFF005BAA)},
-    {'id': 'gopay', 'name': 'gopay', 'color': Color(0xFF00AED6)},
-    {'id': 'ovo', 'name': 'OVO', 'color': Color(0xFF4C2A86)},
-    {'id': 'card', 'name': 'Credit/Debit Card', 'color': Colors.grey},
-  ];
-
-  String _formatRupiah(int amount) {
-    final str = amount.toString();
-    final buffer = StringBuffer();
-    for (int i = 0; i < str.length; i++) {
-      if (i > 0 && (str.length - i) % 3 == 0) buffer.write('.');
-      buffer.write(str[i]);
-    }
-    return 'Rp ${buffer.toString()}';
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
